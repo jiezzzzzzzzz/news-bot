f5f0e02156899300a84b91432286425b7f7034c0
jiezzzzzzzzz <gibson2304@mail.ru>
2022-03-18 19:40:20 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import telebot
+import requests
+from bs4 import BeautifulSoup
+import os
+import time
+
+token = os.environ['TOKEN']
+bot = telebot.TeleBot(os.getenv('TOKEN'))
+channel_id = '@chtotoproishodit'
+# сюда можно вставить ссылку на любой канал
+
+
+url = 'https://vc.ru/new'
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+post = soup.find('div', class_="feed__item l-island-round")
+title = post.find("div", class_="content-container").text.strip()
+post_url = post.find("a", class_="content-link", href=True)["href"].strip()
+
+
+db = []
+db.append(post_url)
+
+
+@bot.message_handler(commands=["start"])
+def main(m):
+    while True:
+        if post_url in db:
+            time.sleep(1800)
+        else:
+            bot.send_message(channel_id, title + post_url)
+            time.sleep(1800)
+
+
+bot.polling(none_stop=True)
+
+# ссылка на канал, куда отправляются новости - https://t.me/chtotoproishodit
+# задеплоено на heroku
