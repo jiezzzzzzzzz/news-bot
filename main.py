e93932183e4ef204dda2be9c1bf27683a2f1870a
jiezzzzzzzzz <gibson2304@mail.ru>
2022-03-19 11:08:35 +0300

    initial commit

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ post = soup.find('div', class_="feed__item l-island-round")
 title = post.find("div", class_="content-container").text.strip()
 post_url = post.find("a", class_="content-link", href=True)["href"].strip()
 
-
-db = []
-db.append(post_url)
+small_db = []
 
 
 @bot.message_handler(commands=["start"])
 def main(m):
     while True:
-        if post_url in db:
+        bot.send_message(channel_id, title + post_url)
+        small_db.append(post_url)
+        if post_url in small_db:
             time.sleep(1800)
         else:
             bot.send_message(channel_id, title + post_url)
